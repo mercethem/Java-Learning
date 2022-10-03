18483806905d6f2d2624a489a56ef0408b0ffb4f
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-10-03 21:40:28 +0300

    Create aggregate.java

---
+++ aggregate.java
@@ -0,0 +1,15 @@
+
+public class Aggregate {
+
+	public static void main(String[] args) {
+		
+		int numberOne = 7;
+		int numberTwo = 2;
+		int aggregate = numberOne + numberTwo;
+		
+		String total = "Total = " + aggregate;
+		System.out.println(total);
+
+	}
+
+}
