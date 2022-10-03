6885c51dfc3f56c643b00cb5632f350e2f9be0d7
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-10-03 19:26:39 +0300

    Create Total.java

---
+++ Total.java
@@ -0,0 +1,14 @@
+
+public class Total {
+
+	public static void main(String[] args) {
+		
+		int numberOne = 7;
+		int numberTwo = 2;
+		int Total = numberOne + numberTwo;
+		
+		System.out.println("Total = " + Total);
+
+	}
+
+}
