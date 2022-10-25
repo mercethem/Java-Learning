21dda83238609cbe1bc592c9a12c1976ff67d5e9
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-10-25 20:12:18 +0300

    Create Exercises13.java

---
+++ LiangBook's Exercises/chapter01/Exercises13.java
@@ -0,0 +1,20 @@
+
+public class Exercises13 {
+
+    public static void main(String[] args) {
+        System.out.println("3.4x + 50.2y = 44.5");
+		System.out.println("2.1x + 55y =  5.9");
+		System.out.println("x = (44.5 * 55) - (50.2 * 5.9)");
+		System.out.println("    ----------------------------");
+		System.out.println("    (3.4  * 55) - (50.2 * 2.1)");
+		System.out.println((((44.5 * 55) - (50.2 * 5.9)) / 
+							 ((3.4 * 55) - (50.2 * 2.1))));
+		System.out.println(" ");
+		System.out.println("y = (3.4 * 5.9) - (44.5 * 2.1)");
+		System.out.println("    --------------------------");
+		System.out.println("    (3.4 * .55) - (50.2 * 2.1)");
+		System.out.println((((3.4 * 5.9) - (44.5 * 2.1)) /
+							((3.4 * 55) - (50.2 * 2.1))));
+        System.out.println();
+    }
+}
