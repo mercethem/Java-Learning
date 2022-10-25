cc81b03124186196ba92ac39fc766bec05faba6d
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-10-25 20:24:34 +0300

    Update Exercises13.java

--- LiangBook's Exercises/chapter01/Exercises13.java
+++ LiangBook's Exercises/chapter01/Exercises13.java
@@ -3,19 +3,17 @@ public class Exercises13 {
 		System.out.println("3.4x + 50.2y = 44.5");
 		System.out.println("2.1x + 55y =  5.9");
         System.out.println();
-		System.out.println("    (44.5 * 55) - (50.2 * 5.9)");
-		System.out.println("x=  --------------------------");
-		System.out.println("    (3.4  * 55) - (50.2 * 2.1)");
+		System.out.println("   (44.5 * 55) - (50.2 * 5.9)");
+		System.out.println("x= --------------------------");
+		System.out.println("   (3.4  * 55) - (50.2 * 2.1)");
         System.out.println();
-		System.out.println((((44.5 * 55) - (50.2 * 5.9)) / 
-							 ((3.4 * 55) - (50.2 * 2.1))));
-		System.out.println(" ");
-		System.out.println("    (3.4 * 5.9) - (44.5 * 2.1)");
-		System.out.println("y=  --------------------------");
-		System.out.println("    (3.4 * 55)  - (50.2 * 2.1)");
+		System.out.println((((44.5 * 55) - (50.2 * 5.9)) / ((3.4 * 55) - (50.2 * 2.1))));
+		System.out.println();
+		System.out.println("   (3.4 * 5.9) - (44.5 * 2.1)");
+		System.out.println("y= --------------------------");
+		System.out.println("   (3.4 * 55) - (50.2 * 2.1)");
         System.out.println();
-		System.out.println((((3.4 * 5.9) - (44.5 * 2.1)) /
-							((3.4 *  55) - (50.2 * 2.1))));
+		System.out.println((((3.4 * 5.9) - (44.5 * 2.1)) / ((3.4 *  55) - (50.2 * 2.1))));
 
     }
 }
