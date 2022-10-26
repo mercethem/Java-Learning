44e73fd399ad81b7a62e83ac7a47504035a814fc
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-10-26 17:33:29 +0300

    Update Exercises09.java

--- LiangBook's Exercises/chapter01/Exercises09.java
+++ LiangBook's Exercises/chapter01/Exercises09.java
@@ -2,7 +2,13 @@
 public class Exercises09 {
 
     public static void main(String[] args) {
-        System.out.print("Area = ");
-        System.out.println( 4.5 * 7.9 );
+        // Changed this section because i learned new codes.
+        /*System.out.print("Area = ");
+        System.out.println("area =" 4.5 * 7.9 );*/
+
+        double number1 = 4.5;
+        double number2 = 7.9;
+        double aggregate = number1 * number2;
+        System.out.println("Aggregate =" + aggregate);
     }
 }
