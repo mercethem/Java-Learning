d2d30611ccc7a6504890edd51c120ae57de73aac
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-10-26 20:01:12 +0300

    Create InputTrial.java

---
+++ InputTrial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class InputTrial {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Please enter your first enter: ");
+        double number1 = input.nextDouble();
+        
+        System.out.println("Please enter your second enter: ");
+        final double number2 =input.nextDouble();
+        
+        System.out.println("This is your numbers total: ");
+        double total = number1 + number2;
+
+        System.out.println(total);
+    }
+}
