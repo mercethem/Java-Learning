1c745f2e7035c53a1c9a6a7fabfaef1f43a12cba
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-10-27 18:45:45 +0300

    Create AverageCalculation.java

---
+++ AverageCalculation.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class AverageCalculation {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter three number for avarage calculation: ");
+        
+        double number1 = input.nextDouble();
+        double number2 = input.nextDouble();
+        double number3 = input.nextDouble();
+        /*Double is not true way (My search said) for avarage because 
+        if you use for this at banks that is problem
+        for after dot line! I will learn later on my way*/
+
+        double avarage = (number1 + number2 + number3)/3;
+        
+        System.out.println("Your avarage calculation: " + avarage);
+
+    
+    }
+}
