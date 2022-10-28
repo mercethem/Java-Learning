269486d0bc5f9b8f5da5cc8f6abe4bb6b438cc73
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-10-28 20:49:31 +0300

    Create Laterals.java

---
+++ Laterals.java
@@ -0,0 +1,31 @@
+
+public class Laterals {
+
+    public static void main(String[] args) {
+        byte b = 127; 
+
+        short s = 32767;
+
+        int i = 2147483647;
+
+        long l = 900000000000000L;
+        // "L" important for long type. Java coders use "L" not "l"...
+
+        float f = 1.0F;
+        // "F" important for float type. Java coders use "F" not "f"...
+
+        double d = 1.0;
+
+        System.out.println(12.356789E1);
+        System.out.println(12.356789E2);
+        System.out.println(12.356789E3);
+        System.out.println(12.356789E+1);
+        System.out.println(12.356789E+2);
+        System.out.println(12.356789E+3);
+        System.out.println(12.356789E-1);
+        System.out.println(12.356789E-4);
+        System.out.println(12.356789E-5);
+        // "E-"Value"" use for Scientific Notation
+
+    }
+}
