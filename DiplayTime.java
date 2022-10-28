07330926328bc115287c0e52bff41518ff79a0b1
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-10-28 17:23:45 +0300

    Create DiplayTime.java

---
+++ DiplayTime.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DiplayTime {
+    public static void main(String[] args) {
+        System.out.println("Please enter minute want to calculate : ");
+
+        Scanner input = new Scanner(System.in);
+
+        int minute = input.nextInt();
+        int calculateMinutes = minute / 60;
+        int calculateSeconds = minute % 60;
+
+        System.out.println("Your want's : " + calculateMinutes + " hours and " + calculateSeconds + " minutes");
+
+    }
+}
