ec979ad697e248e985dc98da0421b43af0208eec
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-10-29 16:04:02 +0300

    29.10.2022

--- DisplayTime.java
+++ DisplayTime.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DiplayTime {
+public class DisplayTime {
     public static void main(String[] args) {
         System.out.println("Please enter minute want to calculate : ");
 
