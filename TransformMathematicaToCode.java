e81a7f65f2a51c7db6dc693422405d1d14640fca
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-10-29 12:26:09 +0300

    29.10.2022

---
+++ TransformMathematicaToCode.java
@@ -0,0 +1,14 @@
+public class TransformMathematicaToCode {
+    public static void main(String[] args) {
+        double x = 1;
+        double y = 1;
+        double a = 1;
+        double b = 1;
+        double c = 1;
+
+        double calculate = ((3 + 4 * x) / 5)
+                - (( 10 * (y - 5) * ( a + b + c)) / x)
+                + 9 * ( (4 / x) + (9 + x) / y);
+        System.out.println("Your calculation is : " + calculate);
+    }
+}
