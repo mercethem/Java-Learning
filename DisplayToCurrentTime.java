41a3244fdb9650a040ca25862665ec2f176f3076
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-10-29 16:06:18 +0300

    29.10.2022

--- DisplayToCurrentTime.java
+++ DisplayToCurrentTime.java
@@ -2,7 +2,7 @@ public class DisplayToCurrentTime {
     public static void main(String[] args) {
         System.out.print("Current Time is ");
         long totalMilliSeconds = System.currentTimeMillis();
-        /* All computers use UNIX epoch and we uses that is for current time.
+        /* All computers use UNIX epoch, we use that is for current time.
         UNIX epoch starts since 00:00 (Time) / 01.01.1970 (Date) */
         long totalSeconds = totalMilliSeconds / 1000;
         long currentSecond = totalSeconds % 60;
