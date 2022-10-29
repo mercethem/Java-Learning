b8eb80ea3082e4ed20dd41599c2d07d7b21d5e9c
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-10-29 19:03:06 +0300

    29.10.2022

---
+++ SomeStaffAtOperators.java
@@ -0,0 +1,40 @@
+
+public class SomeStaffAtOperators {
+    public static void main(String[] args) {
+        int number1 = 10;
+        number1 += 5;
+        System.out.println(number1);
+        int number2 = 5;
+        number2 += 10;
+        System.out.println(number2);
+        int a = 9;
+        int b = 12;
+        ++a;
+        --b;
+        System.out.println(a);
+        System.out.println(b);
+
+        a = 9;
+        b = 12;
+        a = a + 1;
+        b = b - 1;
+        System.out.println(a);
+        System.out.println(b);
+
+        int k = 20;
+        int l = k + 1;
+        System.out.println(k);
+        System.out.println(l);
+
+        int m = 20;
+        int n = m + 1;
+        System.out.println(n);
+        System.out.println(n);
+
+        int f = 10;
+        ++f;
+        int c = f * 20;
+        System.out.println(f);
+        System.out.println(c);
+    }
+}
