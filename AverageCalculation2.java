2fc6ae9238109b4ee76d66a0fe896d2359060f34
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-10-31 20:40:53 +0300

    E.M. commit that

--- AverageCalculation2.java
+++ AverageCalculation2.java
@@ -1,22 +1,22 @@
 
 import java.util.Scanner;
 
-public class AvarageCalculation2 {
+public class AverageCalculation2 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter three number for avarage calculation: ");
+        System.out.println("Enter three number for average calculation: ");
         
         float number1 = input.nextFloat();
         float number2 = input.nextFloat();
         float number3 = input.nextFloat();
 
-        float avarage = (number1 + number2 + number3)/3;
-        /* AvarageCalculation's problem, i used to double but
+        float average = (number1 + number2 + number3)/3;
+        /* AverageCalculation's problem, i used to double but
          * i understand that problem and i changed this double to float.*/
         
-        System.out.println("Your avarage calculation: " + avarage);
+        System.out.println("Your average calculation: " + average);
 
     
     }
