9d213de204633c9d51f015c4fe02a8a2609f10d6
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-03 07:49:27 +0300

    me_et

--- IfElse.java
+++ IfElse.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class IfElse {
     public static void main(String[] args) {
-        int number1 = (int)(Math.random() * 10);
-        int number2 = (int)(Math.random() * 10);
+        int number1 = (int) (Math.random() * 10);
+        int number2 = (int) (Math.random() * 10);
 
         System.out.println("What is answer ? " + number1 + " + " + number2 + " = ");
         Scanner input = new Scanner(System.in);
@@ -11,8 +11,7 @@ public class IfElse {
 
         if (keyboard == number1 + number2) {
             System.out.println("Correct!");
-        }
-        else {
+        } else {
             System.out.println("Wrong!");
         }
     }
