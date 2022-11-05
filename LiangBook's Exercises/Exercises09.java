9b7fdce9304ea73d41e08119c70efc927ebf0235
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-05 19:49:47 +0300

    Create Exercises09.java

---
+++ LiangBook's Exercises/Exercises09.java
@@ -0,0 +1,14 @@
+
+public class Exercises09 {
+
+    public static void main(String[] args) {
+        // Changed this section because i learned new codes.
+        /*System.out.print("Area = ");
+        System.out.println("area =" 4.5 * 7.9 );*/
+
+        double number1 = 4.5;
+        double number2 = 7.9;
+        double result = number1 * number2;
+        System.out.println("Transaction Result = " + result);
+    }
+}
