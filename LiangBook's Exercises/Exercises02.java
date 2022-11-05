cf2eff0dcced75a4e3bfb501552289fdbd5eb7d3
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-05 19:49:35 +0300

    Create Exercises02.java

---
+++ LiangBook's Exercises/Exercises02.java
@@ -0,0 +1,9 @@
+public class Exercises02 {
+    public static void main(String[] args) {
+        System.out.println("Wellcome to Java");
+        System.out.println("Wellcome to Java");
+        System.out.println("Wellcome to Java");
+        System.out.println("Wellcome to Java");
+        System.out.println("Wellcome to Java");
+    }
+}
