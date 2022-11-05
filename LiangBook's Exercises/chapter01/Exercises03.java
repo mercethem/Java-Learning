976f9588cda932b89fa558a0d01aff301f079a83
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-05 19:48:49 +0300

    Delete Exercises03.java

--- LiangBook's Exercises/chapter01/Exercises03.java
+++
@@ -1,9 +0,0 @@
-public class Exercises03 {
-    public static void main(String[] args) {
-        System.out.println(
-                "       J    A   V     V   A        \n" +
-                "       J   A A   V   V   A A        \n" +
-                "   J   J  AAAAA   V V   AAAAA      \n" +
-                "    J J  A     A   V   A     A     "  );
-    }
-}
