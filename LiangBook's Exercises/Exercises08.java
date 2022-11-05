50bcdeef2878b93ae8310580f9f10c4b9ab5ff7e
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-05 19:49:45 +0300

    Create Exercises08.java

---
+++ LiangBook's Exercises/Exercises08.java
@@ -0,0 +1,20 @@
+
+public class Exercises08 {
+  
+    public static void main(String[] args) {
+    
+        System.out.print("Perimeter = ");   
+        System.out.println( 2 * 5.5 * Math.PI );
+        System.out.print("Area = ");
+        System.out.println( 5.5 * 5.5 * Math.PI  );
+    
+        //I have learned double and changed down section
+        double perimeter;
+        double area;
+
+        perimeter = 5.5;
+        area = perimeter*perimeter*Math.PI;
+
+    System.out.println(area);
+    }
+}
