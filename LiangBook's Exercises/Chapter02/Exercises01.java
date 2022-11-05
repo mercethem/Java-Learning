e144167337240e0d027cb3697a40b967980e9c88
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-05 19:38:01 +0300

    me_et

--- LiangBook's Exercises/Chapter02/Exercises01.java
+++
@@ -1,5 +0,0 @@
-public class Exercises01 {
-    public static void main(String[] args) {
-
-    }
-}
--- LiangBook's Exercises/Chapter02/Exercises02.java
+++
@@ -1,5 +0,0 @@
-public class Exercises02 {
-    public static void main(String[] args) {
-
-    }
-}
