d0a964cd73492079dda0feed1e9fbff9cfdd85a5
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-05 19:48:51 +0300

    Delete Exercises04.java

--- LiangBook's Exercises/chapter01/Exercises04.java
+++
@@ -1,6 +0,0 @@
-public class Exercises04 {
-    public static void main(String[] args) {
-        System.out.println("a   a^2 a^3\n" + "1   1   1\n" + 
-        "2   4   8\n" + "3   9   27\n" + "4   16  64");
-    }
-}
