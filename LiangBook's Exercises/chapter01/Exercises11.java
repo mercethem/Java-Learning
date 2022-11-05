406c8577517c07bf217b18c4e1a68c65c640c5d9
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-05 19:49:21 +0300

    Delete Exercises11.java

--- LiangBook's Exercises/chapter01/Exercises11.java
+++
@@ -1,8 +0,0 @@
-
-public class Exercises11 {
-
-    public static void main(String[] args) {
-        System.out.println(312032486+5*((365*24*60*60)/7-(365*24*60*60/13)+(365*24*60*60/45)));
-
-    }
-}
