296010e4c923ac09a41f4a6ff1bac083f9995297
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-05 19:48:41 +0300

    Delete Exercises02.java

--- LiangBook's Exercises/chapter01/Exercises02.java
+++
@@ -1,9 +0,0 @@
-public class Exercises02 {
-    public static void main(String[] args) {
-        System.out.println("Wellcome to Java");
-        System.out.println("Wellcome to Java");
-        System.out.println("Wellcome to Java");
-        System.out.println("Wellcome to Java");
-        System.out.println("Wellcome to Java");
-    }
-}
