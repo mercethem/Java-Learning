ddcb952962bc26506c41401828ad03e7fe95013f
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-06 11:32:41 +0300

    me_et

--- LiangBook's Exercises/Exercises02_02.java
+++ LiangBook's Exercises/Exercises02_02.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Exercises02_02 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a radius and length of cylinder " +
+                            "that want to calculate volume : ");
+        double radius = input.nextDouble();
+        double length = input.nextDouble();
+
+        double area  =  radius * radius * Math.PI;
+        double volume = area * length;
+        System.out.println("The area is " + area);
+        System.out.println("The volume is " + volume);
+
     }
 }
