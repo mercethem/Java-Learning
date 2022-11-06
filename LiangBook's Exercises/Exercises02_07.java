48ae7fd35fd56c3598ac5c40558207446d8c0cf9
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-06 14:09:34 +0300

    me_et

---
+++ LiangBook's Exercises/Exercises02_07.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Exercises02_07 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter the number of minutes for year calculation : ");
+        long minutes = keyboard.nextLong();
+        int years = (int) (((minutes / 60) / 24) / 365);
+        int days = (int) ((minutes % (60 * 24 * 365) / (60*24)));
+
+        System.out.println(years + " year " + days + " days ");
+    }
+}
