2b1f7629a33b25ef587099ae515ee6b5527ea378
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-06 11:39:58 +0300

    me_et

---
+++ LiangBook's Exercises/Exercises02_03.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercises02_03 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a value for feet : ");
+        double feet = keyboard.nextDouble();
+        double a_feet = 0.305;
+        double meter = a_feet * feet;
+        System.out.println(feet + " feet is " + meter + " meters ");
+    }
+}
