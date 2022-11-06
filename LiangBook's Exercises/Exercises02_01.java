27ef1978844ac1fd25d79d393559c42a4249f410
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-06 11:22:31 +0300

    me_et

--- LiangBook's Exercises/Exercises02_01.java
+++ LiangBook's Exercises/Exercises02_01.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class Exercises02_01 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a celsius degree : ");
+        double celsius = input.nextDouble();
+        double fahrenheit = 9.0 / 5 * celsius + 32;
+        System.out.println(celsius + " is " + fahrenheit + " fahrenheit ");
+
     }
 }
