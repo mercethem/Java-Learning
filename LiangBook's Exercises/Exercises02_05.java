72e957f34058e66ccbc48251915a848ec167e4c1
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-06 12:00:30 +0300

    me_et

---
+++ LiangBook's Exercises/Exercises02_05.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercises02_05{
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter the subtotal and a gratuity rate for calculate them : ");
+
+        double subtotal = keyboard.nextDouble();
+        double gratuityRate = keyboard.nextDouble();
+        double gratuity = (subtotal * gratuityRate) / 100;
+        double total = gratuity + subtotal;
+
+        System.out.println("The gratuity is $" + gratuity +  " and total is $" + total);
+
+    }
+
+}
