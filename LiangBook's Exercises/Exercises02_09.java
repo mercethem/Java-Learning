3a18674f1fb90cf28f229c15070eb6f40eb9583e
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-07 07:08:42 +0300

    me_et

--- LiangBook's Exercises/Exercises02_09.java
+++ LiangBook's Exercises/Exercises02_09.java
@@ -1,3 +1,6 @@
+
+//Physics: acceleration)
+
 import java.util.Scanner;
 
 public class Exercises02_09 {
@@ -5,11 +8,11 @@ public class Exercises02_09 {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Please enter to values v0, v1 and t for calculate acceleration : ");
 
-        float v0 = keyboard.nextFloat(); // v0 is first speed
-        float v1 = keyboard.nextFloat(); // v1 is last speed
-        float t = keyboard. nextFloat(); // t is time change
-        float a = (v1 - v0) / t ; //a is acceleration
-        System.out.println(a);
+        float initialSpeed = keyboard.nextFloat();
+        float finishSpeed = keyboard.nextFloat();
+        float time = keyboard. nextFloat();
+        float acceleration = (finishSpeed - initialSpeed) / time ;
+        System.out.println(acceleration);
 
     }
 }
--- LiangBook's Exercises/Exercises02_10.java
+++ LiangBook's Exercises/Exercises02_10.java
@@ -1,3 +1,20 @@
-public class Exercises02_10 {
 
+//Science: Calculating energy
+
+import java.util.Scanner;
+
+public class Exercises02_10 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter the amount of water in kilograms : ");
+        float amountOfWater = keyboard.nextFloat();
+        System.out.println("Enter the initial temperature");
+        float initialTemperature = keyboard.nextFloat();
+        System.out.println("Enter the final temperature");
+        float finalTemperature = keyboard.nextFloat();
+        float neededEnergy = amountOfWater * ( finalTemperature - initialTemperature) * 4184 ;
+        System.out.println(neededEnergy);
+
+
+    }
 }
