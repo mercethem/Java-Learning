16c5ca6f72f6e0597bf6570ee9f53e9724ac4e42
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-08 09:00:01 +0300

    me_et

---
+++ LiangBook's Exercises/Exercises02_13.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//Financial application: compound value
+public class Exercises02_13 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please enter the monthly saving amount: ");
+        float monthlySaving = keyboard.nextFloat();
+
+        System.out.println(" Please enter savings percentage (e.g 5 ): ");
+        float percentage = keyboard.nextFloat();
+
+        float monthPercentage = (float)((percentage * 0.01) / 12);
+        float totalSaving;
+        totalSaving = monthlySaving * (1 + monthPercentage);
+        totalSaving = (totalSaving + monthlySaving) + (1 + monthPercentage);
+        totalSaving = (totalSaving + monthlySaving) + (1 + monthPercentage);
+        totalSaving = (totalSaving + monthlySaving) + (1 + monthPercentage);
+        totalSaving = (totalSaving + monthlySaving) + (1 + monthPercentage);
+        totalSaving = (totalSaving + monthlySaving) + (1 + monthPercentage);
+        System.out.println(totalSaving);
+    }
+}
---
+++ LiangBook's Exercises/Exercises02_14.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercises02_14 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please enter your pounds : ");
+        float mass = keyboard.nextFloat();
+
+        System.out.println("Please enter your inches : ");
+        float inches = keyboard.nextFloat();
+
+        float bmi = (float)(mass / (inches * inches) * 703.06957964);
+
+        System.out.println(bmi);
+    }
+}
