6ab3fe57268a89d96b9a70d590964d3b02b92049
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-12 18:26:30 +0300

    me_et

--- DrawATrianle.java
+++ DrawATrianle.java
@@ -7,11 +7,14 @@ public class DrawATrianle {
         int r = keyboard.nextInt();
         int i;
         int j;
-        for (i=0; i<=r; i++){
-            for (j=1; j<=r-i; j++){
+        for (i = 0; i <= r; i++) {
+            for (j = 1; j <= r - i; j++) {
                 System.out.print("");
             }
-            for (j=1; j<=2*i-1; j++){
+            for (j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
-            System.out.print("\n");}}}
+            System.out.print("\n");
+        }
+    }
+}
