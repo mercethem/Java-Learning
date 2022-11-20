c6ddc90a5f4930443915aeb91805abaebeb1e6cb
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-11-20 17:00:34 +0300

    Re-format

--- MathRandom.java
+++ MathRandom.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class MathRandom {
     public static void main(String[] args) {
-        System.out.println((byte) (Math.random() * 10));
+
+        System.out.println(Math.random());
     }
 }
