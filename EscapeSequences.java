9740fae7b301370a36ca381b14bbc1ac8fa7154b
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-02 10:36:33 +0300

    About Escape Sequences

---
+++ EscapeSequences.java
@@ -0,0 +1,15 @@
+public class EscapeSequences {
+    public static void main(String[] args) {
+        System.out.println("Hello \"java\" world!");
+        System.out.println("Hello\b java\b world!");
+        System.out.println("Hello\tjava\tworld!");
+        System.out.println("Hello\njava\nworld!");
+        System.out.println("Hello\fjava\fworld!");
+        System.out.println("Hello\rjava\rworld!");
+        System.out.println("Hello\\java\\world!");
+        
+
+
+
+    }
+}
