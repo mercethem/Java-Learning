85b3e04a8a14aa272d907a0ebad3fffd66fc96cc
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-06 16:55:39 +0300

    Created about Characters and digits and Up/Down cases

---
+++ MethodsInTheCharacterClass.java
@@ -0,0 +1,12 @@
+public class MethodsInTheCharacterClass {
+    public static void main(String[] args) {
+
+        System.out.println("isdigit('a') is " + Character.isDigit('a'));
+        System.out.println("isletter('a') is " + Character.isLetter('a'));
+        System.out.println("islowercase('a') is " + Character.isLowerCase('a'));
+        System.out.println("isuppercase('a') is " + Character.isUpperCase('a'));
+        System.out.println("touppercase('a') is " + Character.toUpperCase('a'));
+        System.out.println("toulowercase('Q') is " + Character.toLowerCase('Q'));
+
+    }
+}
