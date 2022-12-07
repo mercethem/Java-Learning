b5af323ca9d0085f1028b78dfcdd640bdf0b3e5a
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-07 09:03:50 +0300

    Created about calculate interest

---
+++ LiangBook's Exercises/Exercises02_20.java
@@ -0,0 +1,16 @@
+//Financial application: Calculate interest
+import java.util.Scanner;
+
+public class Exercises02_20 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter balance and annual interest rate (e.g. 3 for %3) : ");
+        float balance = keyboard.nextFloat();
+        float annualInterestRate = keyboard.nextFloat();
+        float interest =balance * ( annualInterestRate/1200);
+        System.out.println(interest);
+
+
+
+    }
+}
