5653a1fd0d3008566b6b88bbf9dce0742f5888c4
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-07 09:02:50 +0300

    Created about print exercises

---
+++ LiangBook's Exercises/Exercises02_18.java
@@ -0,0 +1,23 @@
+//Print a table
+
+public class Exercises02_18 {
+    public static void main(String[] args) {
+
+        int a1 = 1, a2 = 2, a3 = 3, a4 = 4, a5 = 5;
+        int b1 = 2, b2 = 3, b3 = 4, b4 = 5, b5 = 6;
+        Math.pow(a1, b1);
+        Math.pow(a2, b2);
+        Math.pow(a3, b3);
+        Math.pow(a4, b4);
+        Math.pow(a5, b5);
+        System.out.println((int)Math.pow(a1, b1));
+        System.out.println((int)Math.pow(a2, b2));
+        System.out.println((int)Math.pow(a3, b3));
+        System.out.println((int)Math.pow(a4, b4));
+        System.out.println((int)Math.pow(a5, b5));
+
+
+
+    }
+
+}
