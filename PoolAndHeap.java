3b6d4db1a19afe284a3a475477692c710e05f16e
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-07 20:41:43 +0300

    Created about pool and heap using, literal and new object

--- PoolAndHeap.java
+++ PoolAndHeap.java
@@ -3,6 +3,6 @@ public class PoolAndHeap {
         String str1 = "Java"; //1 slot use in pool
         String str2 = "Java"; //Do not use any slot in the pool because uses same slot mentioned above
         String str3 = new String ("Java"); //   This is different from the mentioned above situations because
-                                                    //  this is new object and uses HEAP not POOL!!
+                                                    //  this is new object and uses HEAP not POOL!!!
     }
 }
