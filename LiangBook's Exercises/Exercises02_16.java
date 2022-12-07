79359d7d0671d3466323b550949cba0f2fc9951b
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-07 09:01:32 +0300

    Created about Area of a hexagon

--- LiangBook's Exercises/Exercises02_16.java
+++ LiangBook's Exercises/Exercises02_16.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
+//Geometry: Area of a hexagon
 public class Exercises02_16 {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter the side : ");
+        double area;
+        double side = keyboard.nextDouble();
+        area = (float) (((3 * Math.sqrt(3)) / 2.0) * (side * side));
+        System.out.println("The area of the hexagon is : " + area);
 
     }
 }
