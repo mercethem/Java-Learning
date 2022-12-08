d4d58e07629c48cde16784c2bd234813f753387d
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-08 06:57:24 +0300

    About root's sum

---
+++ LiangBook's Exercises/Exercises03_02.java
@@ -0,0 +1,11 @@
+//Game: root's sum
+import import1.Exercises03_01;
+
+public class Exercises03_02{
+    public static void main(String[] args) {
+        Exercises03_01 x = new Exercises03_01();
+        System.out.println(x);
+     //   System.out.println("Other r1 and r2 values sum; " + r1 + r2);
+        /***This exercise will modify later
+    }
+}
