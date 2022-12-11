87453abd36782310b14e1e3d4bdd029260735c7c
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-11 10:50:25 +0300

    Re-format

--- FindTypeOfWebsiteAndProtocolUsed.java
+++ FindTypeOfWebsiteAndProtocolUsed.java
@@ -7,28 +7,28 @@ public class FindTypeOfWebsiteAndProtocolUsed {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Please enter your website : ");
         String url = keyboard.nextLine();
-        if (url.contains(".com")){
+        if (url.contains(".com")) {
             System.out.println("Website's type is Commercial");
         } else if (url.contains(".gov")) {
             System.out.println("Website's type is Government");
-        } else if (url.contains(".org")){
+        } else if (url.contains(".org")) {
             System.out.println("Website's type is Organisation");
         } else if (url.contains(".net")) {
             System.out.println("Website's type is Network");
-        } else if (url.contains(".biz")){
+        } else if (url.contains(".biz")) {
             System.out.println("Website's type is Business");
         } else {
             System.out.println("Not Found website's type");
         }
-        if(url.contains("http")){
+        if (url.contains("http")) {
             System.out.println("Protocol is: HypeText Transfer Protocol");
-        } else if (url.contains("https")){
+        } else if (url.contains("https")) {
             System.out.println(" Protocol is: HyperText Transfer Protocol Secure");
-        } else if (url.contains("ftp")){
+        } else if (url.contains("ftp")) {
             System.out.println(" Protocol is: File Transfer Protocol");
-        } else if (url.contains("pop3")){
+        } else if (url.contains("pop3")) {
             System.out.println(" Protocol is: Post Office Protocol");
-        } else if (url.contains("SMTP")){
+        } else if (url.contains("SMTP")) {
             System.out.println(" Protocol is: Simple Mail Transfer Protocol");
         } else {
             System.out.println("Nan not found protocol!");
