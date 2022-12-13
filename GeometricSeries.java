a86d2895274a3d29baf555ef11e46664c1eaaa16
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-13 09:27:24 +0300

    Created for geometric series

---
+++ GeometricSeries.java
@@ -0,0 +1,17 @@
+//Geometric series
+import java.util.Scanner;
+
+public class GeometricSeries {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int start = keyboard.nextInt();
+        int number = keyboard.nextInt();
+        int geometric = keyboard.nextInt();
+
+
+        for (int i = 1; i<=number; i++){
+            start = start*geometric;
+            System.out.println(start);
+        }
+    }
+}
