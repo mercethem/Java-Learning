c6e9cf4c518598b12a697fd1b3348475a67d5793
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-13 17:56:43 +0300

    Created for drawing matrix

---
+++ Matrix.java
@@ -0,0 +1,18 @@
+//Draw matrix
+import java.util.Scanner;
+
+public class Matrix {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter an integer for line : ");
+        System.out.println("Please enter an integer for column : ");
+        int line = keyboard.nextInt();
+        int column = keyboard.nextInt();
+        for (int i=1; i<=column ;i++){
+            for(int j=1; j<=line ;j++){
+                System.out.print(" ( "+ i + " , " + j + " ) ");
+            }
+            System.out.println();
+        }
+    }
+}
