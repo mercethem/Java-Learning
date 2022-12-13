deae8126eb6dd4c8b9c12ad966718aa65e582622
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-13 17:56:12 +0300

    Created for drawing pattern

---
+++ Pattern6.java
@@ -0,0 +1,18 @@
+//Draw pattern
+import java.util.Scanner;
+
+public class Pattern6 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter an integer for draw pattern : ");
+        int number = keyboard.nextInt();
+        int counter=1;
+        for (int i = 1; i <= number+counter; i++,number--) {
+            for (int j = number; 1 <=j ; j--) {
+                System.out.printf("%02d ",counter);
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+}
