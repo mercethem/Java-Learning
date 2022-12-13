10782ce537c69e7e152ccd735011de8062afbba6
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-13 08:51:57 +0300

    Created for finding sum of numbers

---
+++ FindSumOfNumbers.java
@@ -0,0 +1,18 @@
+//Finding sum of numbers
+import java.util.Scanner;
+
+public class FindSumOfNumbers {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter an integer : ");
+        int number = keyboard.nextInt();
+        long sum = 0;
+
+        for (int i = 0 ; i <= number; i++) {
+            sum=sum+i;
+
+
+        }
+        System.out.println(sum);
+    }
+}
