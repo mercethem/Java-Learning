579a23ca6597f19b3b3e2283506d4dcc819d28e5
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-15 15:39:01 +0300

    Created for insert a value to array

---
+++ InsertAElementToArray.java
@@ -0,0 +1,32 @@
+//Insert a value to array
+import java.util.Scanner;
+
+public class InsertAElementToArray {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please enter an integer : ");
+        int number = keyboard.nextInt();
+
+        int A[] = new int[10];
+        A[0] = 3;
+        A[1] = 9;
+        A[2] = 7;
+        A[3] = 8;
+        A[4] = 12;
+        A[5] = 6;
+
+        System.out.println("Place enter a sequence in array want to insert : ");
+        int placeInArray = keyboard.nextInt();
+
+        for (int i =A.length-placeInArray ; i >= placeInArray; i--) {
+            A[i + 1] = A[i];
+        }
+        A[placeInArray]=number;
+
+        for (int i = 0; i < A.length; i++) {
+            System.out.print(A[i] + ",");
+        }
+        System.out.println("");
+    }
+}
