00d90ce9f61181496691fa638c8e574528364ef5
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-15 15:40:46 +0300

    Created for finding max element of array

---
+++ MaxElementOfArray.java
@@ -0,0 +1,15 @@
+//Finding max element of array
+public class MaxElementOfArray {
+    public static void main(String[] args) {
+        int A[] = {3, 9, 7, 8, 12, 6, 15, 5, 4, 10};
+        int max1 = A[0];
+        int counter = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > max1) {
+                max1 = A[i];
+            }
+
+        }
+        System.out.println("Largest element is : "+max1);
+    }
+}
