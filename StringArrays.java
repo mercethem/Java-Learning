80a63f9a8f756c63e8a043d265beb16c17a5f46f
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-16 16:29:07 +0300

    Created for sorting to string array

---
+++ StringArrays.java
@@ -0,0 +1,12 @@
+public class StringArrays {
+    public static void main(String[] args) {
+        String arr[] = {"java", "python", "pascal", "smalltalk", "ada", "javascript"};
+        java.util.Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+
+        }
+
+
+    }
+}
