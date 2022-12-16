2556174b46bd19f8eca3a16d2fc729ed9b2729c3
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-16 13:50:12 +0300

    Created for reverse copying an array

---
+++ ReverseCopyingAnArray.java
@@ -0,0 +1,23 @@
+public class ReverseCopyingAnArray {
+    public static void main(String[] args) {
+        int A[] = new int[10];
+        int B[] = new int[10];
+        A[0] = 8;
+        A[1] = 6;
+        A[2] = 10;
+        A[3] = 9;
+        A[4] = 2;
+        A[5] = 15;
+        A[6] = 7;
+        A[7] = 13;
+        A[8] = 14;
+        A[9] = 11;
+        for (int i = 0; i < A.length; i++) {
+            B[(A.length-1)-i]=A[i];
+        }
+
+        for (int i = 0; i <B.length ; i++) {
+            System.out.print(B[i] + " ");
+        }
+    }
+}
