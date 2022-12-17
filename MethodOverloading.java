b7e308d20f4f684ae709435d60096f504b20eac3
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-17 20:06:03 +0300

    Created for methods overloading

---
+++ MethodOverloading.java
@@ -0,0 +1,19 @@
+//Similar methods about overloading
+public class MethodOverloading {
+
+    static int max(int x, int y) {
+        return x > y ? x : y;
+    }
+
+    static float max(float x, float y) {
+        return x > y ? x : y;
+    }
+
+    static int max(int x, int y, int z) {
+        return x > y & x > z ? x : (y > z ? y : z);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(max(1,7,1));
+    }
+}
