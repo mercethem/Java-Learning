f2967bfc5f3c0107bba5f77a5fdd21f19f825bd8
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-17 23:23:25 +0300

    Re-Format

--- CharAndAscii.java
+++ CharAndAscii.java
@@ -15,22 +15,22 @@ public class CharAndAscii {
         char ch3 = 0x0041;
         System.out.println(ch3);
 
-        char ch4 = (char)66.90;
-        char ch5 = (char)67.0;
+        char ch4 = (char) 66.90;
+        char ch5 = (char) 67.0;
         System.out.println(ch4);
         System.out.println(ch5);
 
         i = '2' + '3';
         System.out.println(i);
 
-        System.out.println("Hello " + 'w' + 'o' + 'r'+'l'+'d' );
+        System.out.println("Hello " + 'w' + 'o' + 'r' + 'l' + 'd');
         System.out.println("Hello " + '9');
-        ch= 'Y';
-        if ('A' <= ch  && ch <= 'Z' ){
+        ch = 'Y';
+        if ('A' <= ch && ch <= 'Z') {
             System.out.println(ch + " is an uppercase letter.");
-        } else if ('a'<= ch && ch <='z') {
+        } else if ('a' <= ch && ch <= 'z') {
             System.out.println(ch + " is a lowercase letter.");
-        } else if ('0'<= ch && ch <= '9') {
+        } else if ('0' <= ch && ch <= '9') {
             System.out.println(ch + " is a numeric character.");
         }
 
