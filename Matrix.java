2fa95764d9d3dcbf8bd0e5ec756a2023c3a66102
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-18 17:39:08 +0300

    Re-Format

--- Matrix.java
+++ Matrix.java
@@ -1,4 +1,5 @@
 //Draw matrix
+
 import java.util.Scanner;
 
 public class Matrix {
@@ -8,9 +9,9 @@ public class Matrix {
         System.out.println("Please enter an integer for column : ");
         int line = keyboard.nextInt();
         int column = keyboard.nextInt();
-        for (int i=1; i<=column ;i++){
-            for(int j=1; j<=line ;j++){
-                System.out.print(" ( "+ i + " , " + j + " ) ");
+        for (int i = 1; i <= column; i++) {
+            for (int j = 1; j <= line; j++) {
+                System.out.print(" ( " + i + " , " + j + " ) ");
             }
             System.out.println();
         }
