39b52b2e0e78d0cc19264e74c8539febd904c777
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-19 22:11:50 +0300

    Created for finding perimeter and area of rectangles with OOP

---
+++ Object_Oriented_Programming/Rectangle.java
@@ -0,0 +1,32 @@
+//Created for rectangle's finding area and perimeter. This class and others in this package's OOP's learning
+
+package Object_Oriented_Programming;
+
+import java.util.Scanner;
+
+class Rectangle {
+    public double breadth;
+    public double length;
+
+    static double area(double length, double breadth) {
+        return length * breadth;
+    }
+
+    static double perimeter(double length, double breadth) {
+        return 2 * (length + breadth);
+    }
+
+}
+
+class RectangleTest {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter a length : ");
+        double length = keyboard.nextDouble();
+        System.out.println("Please enter a breadth : ");
+        double breadth = keyboard.nextDouble();
+        System.out.println("Area is : " + Rectangle.area(length, breadth));
+        System.out.println("Perimeter is " + Rectangle.perimeter(length, breadth));
+    }
+}
