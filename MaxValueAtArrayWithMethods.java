33db4240cb01dbac648462fc00367bd30659280c
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-19 07:32:03 +0300

    Created for finding max value in array with method

---
+++ MaxValueAtArrayWithMethods.java
@@ -0,0 +1,18 @@
+//About finding max value in array with using method
+public class MaxValueAtArrayWithMethods {
+    static int arr(int ...A) {
+        for (int i = 0; i <A.length-1 ; i++) {
+            if(A[i]>A[i+1]){
+               int temp = A[i];
+               A[i]=A[i+1];
+               A[i+1]=temp;
+            }
+        }
+        return A[A.length-1];
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(arr(1,2,3,4,354,564,21,284,999,-2000));
+    }
+}
