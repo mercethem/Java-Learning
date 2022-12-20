a9cbc5458cc983fa787cf17199df984444fc9167
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-20 10:30:15 +0300

    Re-format

--- Liang_Books_Exercises/Exercises03_03.java
+++ Liang_Books_Exercises/Exercises03_03.java
@@ -1,4 +1,5 @@
 //Algebra: solve 2 * 2 linear equations
+
 import java.util.Scanner;
 
 public class Exercises03_03 {
@@ -12,7 +13,7 @@ public class Exercises03_03 {
         double e = keyboard.nextDouble();
         double f = keyboard.nextDouble();
 
-        if ( ((a * d) - (b * c)) != 0) {
+        if (((a * d) - (b * c)) != 0) {
 
             double x = ((e * d) - (b * f)) / ((a * d) - (b * c));
             double y = ((a * f) - (e * c)) / ((a * d) - (b * c));
