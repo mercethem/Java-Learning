f62aaba72f34df38d218c45d756cb6add68123d9
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-20 10:30:21 +0300

    Re-format

--- Liang_Books_Exercises/Exercises03_04.java
+++ Liang_Books_Exercises/Exercises03_04.java
@@ -1,42 +1,42 @@
 //Find random month
 public class Exercises03_04 {
     public static void main(String[] args) {
-        int i = (int) (((Math.random())*1200/100+1));
+        int i = (int) (((Math.random()) * 1200 / 100 + 1));
         switch (i) {
-            case 1 :
+            case 1:
                 System.out.println("January");
                 break;
-            case 2 :
+            case 2:
                 System.out.println("February");
                 break;
-            case 3 :
+            case 3:
                 System.out.println("March");
                 break;
-            case 4 :
+            case 4:
                 System.out.println("April");
                 break;
-            case 5 :
+            case 5:
                 System.out.println("May");
                 break;
-            case 6 :
+            case 6:
                 System.out.println("June");
                 break;
-            case 7 :
+            case 7:
                 System.out.println("July");
                 break;
-            case 8 :
+            case 8:
                 System.out.println("August");
                 break;
-            case 9 :
+            case 9:
                 System.out.println("September");
                 break;
-            case 10 :
+            case 10:
                 System.out.println("October");
                 break;
-            case 11 :
+            case 11:
                 System.out.println("November");
                 break;
-            case 12 :
+            case 12:
                 System.out.println("December");
                 break;
 
