47f9db3078d1803aba3532926095fcca23cc6ee9
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-20 16:17:23 +0300

    Created for circe's calculations with OOP

---
+++ Object_Oriented_Programming/Circle.java
@@ -0,0 +1,34 @@
+//Finding about a circle's calculations with OOP
+package Object_Oriented_Programming;
+
+import java.util.Scanner;
+
+public class Circle {
+    static double radius;
+    static double height;
+
+    static double lidArea(double radius, double height) {
+        return Math.PI * radius * radius;
+    }
+
+    static double totalSurfaceArea(double radius, double height) {
+        return (2 * Math.PI * Math.pow(radius, 2) * height) + 2 * Math.PI * Math.pow(radius, 2);
+    }
+
+    static double volume(double radius, double height) {
+        return (Math.PI * radius * radius * height);
+    }
+}
+
+ class Circletest {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter radius : ");
+        double radius = keyboard.nextDouble();
+        System.out.println("Please enter height : ");
+        double height = keyboard.nextDouble();
+        System.out.println("Lid Area is : " + Circle.lidArea(radius, height) + "\n" +
+                "Total surface area is : " + Circle.totalSurfaceArea(radius, height) + "\n" +
+                "Volume is : " + Circle.volume(radius, height));
+    }
+}
