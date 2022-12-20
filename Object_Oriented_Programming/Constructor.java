d0bd9c1e9675697c927b69d19110f15b8e9b1b6a
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-20 20:55:21 +0300

    Created for learning about Constructor

---
+++ Object_Oriented_Programming/Constructor.java
@@ -0,0 +1,29 @@
+//About constructor in java programming
+package Object_Oriented_Programming;
+
+
+class Constructor {
+    private double length;
+    private double breadth;
+
+    public Constructor() {
+        length=1;
+        breadth=1;
+    }
+    public Constructor(double l, double b){
+        length = l;
+        breadth = b;
+    }
+    public Constructor(double s){
+        length=breadth=s;
+    }
+}
+class ConstructorTest{
+    public static void main(String[] args) {
+        Constructor r = new Constructor();
+        /** if enter two value or one value or don't any value that result is different constructor usable for result
+         * first constructor is for do not any value, second one is two value and last one is just one value */
+
+
+    }
+}
