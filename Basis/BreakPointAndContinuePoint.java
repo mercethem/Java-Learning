d74e0c6bd91976293c64521c58a05fbd1c782074
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-21 18:39:15 +0300

    Added continue and just break line

---
+++ Basis/BreakPointAndContinuePoint.java
@@ -0,0 +1,26 @@
+//About break point.
+package Basis;
+
+public class BreakPointAndContinuePoint {
+    public static void main(String[] args) {
+        aa:
+        for (int i = 1; i <= 4; i++) {
+            bb:
+            for (int j = 1; j <= 4; j++) {
+                if (i == 2 && j == 2) {
+                    break bb;
+                }
+
+                System.out.println(i + " " + j);
+                if (j == 3) {
+                    break;
+
+                }
+                if (j == 2) {
+                    continue;
+                }
+
+            }
+        }
+    }
+}
