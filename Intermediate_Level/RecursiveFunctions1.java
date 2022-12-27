7e43349bccb5e843d40ed7c3e4440e338760d1a9
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-27 20:13:12 +0300

    Created for number addition done using the recursive function

---
+++ Intermediate_Level/RecursiveFunctions1.java
@@ -0,0 +1,19 @@
+//About 0-->your number addition done using the recursive function
+package Intermediate_Level;
+
+import java.util.Scanner;
+
+public class RecursiveFunctions1 {
+    public static int addition(int n){
+        if (n==1) return 1;
+        return n+addition(n-1);
+    }
+
+    public static void main(String[] args) {
+       Scanner keyboard = new Scanner(System.in);
+        int number = keyboard.nextInt();
+        System.out.println(addition(number));
+
+        }
+    }
+
