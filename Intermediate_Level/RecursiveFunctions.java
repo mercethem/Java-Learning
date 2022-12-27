5a8a2139e3bec7a631034a02ca301ebc7bce269d
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-27 19:38:18 +0300

    Created, this project is done using recursive function.

---
+++ Intermediate_Level/RecursiveFunctions.java
@@ -0,0 +1,24 @@
+// This is about fibonacci
+package Intermediate_Level;
+
+import java.util.Scanner;
+
+public class RecursiveFunctions {
+    public static int fibonacci(int n){
+        if (n==0) return 1;
+        if (n==1) return 1;
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter an integer want to learn addition with fibonacci :");
+        int number = keyboard.nextInt();
+        for (int i = 0; i <number ; i++) {
+            System.out.printf("%s,",fibonacci(i));
+        }
+
+
+        System.out.println(fibonacci(number));
+    }
+}
