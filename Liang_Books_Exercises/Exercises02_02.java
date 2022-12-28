ed248d9c1ba950cbccc56b71bff4e249524aeea2
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-28 07:21:47 +0300

    Re-format

--- Liang_Books_Exercises/Exercises02_02.java
+++ Liang_Books_Exercises/Exercises02_02.java
@@ -4,11 +4,11 @@ public class Exercises02_02 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter a radius and length of cylinder " +
-                            "that want to calculate volume : ");
+                "that want to calculate volume : ");
         double radius = input.nextDouble();
         double length = input.nextDouble();
 
-        double area  =  radius * radius * Math.PI;
+        double area = radius * radius * Math.PI;
         double volume = area * length;
         System.out.println("The area is " + area);
         System.out.println("The volume is " + volume);
