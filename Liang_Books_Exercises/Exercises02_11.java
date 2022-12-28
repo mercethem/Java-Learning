f2028366aa26212c744170f40c41b1c7b16705b4
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-28 07:22:06 +0300

    Re-format

--- Liang_Books_Exercises/Exercises02_11.java
+++ Liang_Books_Exercises/Exercises02_11.java
@@ -6,8 +6,8 @@ public class Exercises02_11 {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Please enter a year want to calculate: ");
         int year = keyboard.nextInt();
-        System.out.println  (312032486 + year *
-                (int)((365 * 24 * 60 * 60) / 7 -
+        System.out.println(312032486 + year *
+                (int) ((365 * 24 * 60 * 60) / 7 -
                         (365 * 24 * 60 * 60 / 13) +
                         (365 * 24 * 60 * 60 / 45)));
 
