2e7f8e93827d92a466c54f2ab3815144464d9776
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-01-02 17:08:39 +0300

    Reformat

--- Intermediate_Level/RecursiveFunctions1.java
+++ Intermediate_Level/RecursiveFunctions1.java
@@ -4,16 +4,16 @@ package Intermediate_Level;
 import java.util.Scanner;
 
 public class RecursiveFunctions1 {
-    public static int addition(int n){
-        if (n==1) return 1;
-        return n+addition(n-1);
+    public static int addition(int n) {
+        if (n == 1) return 1;
+        return n + addition(n - 1);
     }
 
     public static void main(String[] args) {
-       Scanner keyboard = new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in);
         int number = keyboard.nextInt();
         System.out.println(addition(number));
 
-        }
     }
+}
 
