275dc39742843f8a6e0861b91b0f5b8f2928b6fe
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-01-04 12:29:48 +0300

    Reformat

--- Object_Oriented_Programming/Circle/Circle.java
+++ Object_Oriented_Programming/Circle/Circle.java
@@ -1,21 +1,27 @@
 //Finding about a circle's calculations with OOP
 package Object_Oriented_Programming.Circle;
+
 import java.util.Scanner;
+
 class Circle {
     private static double radius;
     private static double height;
+
     static double lidArea(double radius, double height) {
 
         return Math.PI * radius * radius;
     }
+
     static double totalSurfaceArea(double radius, double height) {
         return (2 * Math.PI * Math.pow(radius, 2) * height) + 2 * Math.PI * Math.pow(radius, 2);
     }
+
     static double volume(double radius, double height) {
 
         return (Math.PI * radius * radius * height);
     }
 }
+
 class CircleTest {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
