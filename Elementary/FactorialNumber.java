26f4307746cf442efc01da064645af83e71f0dbd
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-01-10 07:26:56 +0300

    Reformat

--- Elementary/FactorialNumber.java
+++ Elementary/FactorialNumber.java
@@ -1,13 +1,14 @@
+//Factorial
+
 import java.util.Scanner;
 
-//Factorial
 public class FactorialNumber {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Please enter an integer : ");
         int number = keyboard.nextInt();
         long result = 1;
-        for (int i = 0 ; i < number; i++, result *=i) {
+        for (int i = 0; i < number; i++, result *= i) {
 
         }
         System.out.println(result);
