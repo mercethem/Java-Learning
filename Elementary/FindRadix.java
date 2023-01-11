1f3e9ab158563b617bd2a6b7dbe5a331a5169b5c
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-01-11 08:34:03 +0300

    Reformat

--- Elementary/FindRadix.java
+++ Elementary/FindRadix.java
@@ -9,13 +9,13 @@ public class FindRadix {
         System.out.println("Please enter your number : ");
         number = keyboard.nextLine();
 
-        if (number.matches("[0-1]+")){
+        if (number.matches("[0-1]+")) {
             System.out.println("Binary Radix (2)");
         } else if (number.matches("[0-7]+")) {
             System.out.println("Octal Radix (8)");
-        } else if (number.matches("[0-9]+")){
+        } else if (number.matches("[0-9]+")) {
             System.out.println("Decimal Radix (10) ");
-        } else if (number.matches("[0-9,A-F]+")){
+        } else if (number.matches("[0-9,A-F]+")) {
             System.out.println("Hexadecimal Radix (16)");
         } else {
             System.out.println("That is not a number!");
