48000dab147054ecb6ebd15bfcba5b00255685c4
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-01-17 17:00:19 +0300

    Reformat

--- Elementary/IncreasingSizeOfArray.java
+++ Elementary/IncreasingSizeOfArray.java
@@ -3,11 +3,11 @@ public class IncreasingSizeOfArray {
     public static void main(String[] args) {
 
         int A[] = {8, 6, 10, 9, 2, 15, 7, 13, 14, 11};
-        int B[] = new int [A.length*2];
-        for (int i = 0; i <A.length ; i++) {
-            B[i]=A[i];
+        int B[] = new int[A.length * 2];
+        for (int i = 0; i < A.length; i++) {
+            B[i] = A[i];
         }
-        for (int i = 0; i <B.length ; i++) {
+        for (int i = 0; i < B.length; i++) {
             System.out.print(B[i] + " ");
 
         }
