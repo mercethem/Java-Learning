5066906937c2a07a96a459e122bcdf0049826a4c
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-01-18 20:41:20 +0300

    Initial commit

---
+++ Elementary/SchoolLesson.java
@@ -0,0 +1,11 @@
+//About for loop how can upper value "i"
+package Elementary;
+
+public class SchoolLesson {
+    public static void main(String[] args) {
+
+        for (double i = 0; i < 1; i +=0.01) {
+            System.out.print(i + " ");
+        }
+    }
+}
